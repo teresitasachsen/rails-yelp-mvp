0f50704c312da9b556eab415fe70ce68b647d79c
teresitasachsen <teresitasachsen99@gmail.com>
2021-02-17 16:27:30 -0600

    finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,6 +25,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :adress, :phone_number, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
